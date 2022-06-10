09928b98417c4d367e5857d80d0e70874c7b46b7
LeafFresh <80403436+LeafFresh@users.noreply.github.com>
2022-06-10 14:20:32 +0200

    Update

---
+++ Garage/pages/home.php
@@ -0,0 +1,21 @@
+<?php
+
+?>
+
+
+
+<img src="images/GTAcustoms.webp" class="LosSanto">
+        
+        <div class="headtext">
+            
+            <h1>Laat je autu bij ons repareren!!</h1>
+                <p>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br> 
+                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,<br> 
+                    when an unknown printer took a galley of type and scrambled it to make a type specimen book.<br> 
+                    It has survived not only five centuries, but also the leap into electronic typesetting,<br> 
+                    remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,<br> 
+                    and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.<br> 
+                </p>
+        </div>
+    <button class="buttonplace">Maak een afspraak</button>
